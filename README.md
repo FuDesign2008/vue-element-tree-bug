ce562675d8f7955d100d15fdf1d74d11ae45787f
fudesign2008 <fudesign2008@163.com>
2018-01-16 14:13:17 +0800

    new file:   .babelrc   new file:   .editorconfig   new file:   .gitignore   modified:   README.md   new file:   error.png   new file:   index.html   new file:   package-lock.json   new file:   package.json   new file:   src/App.vue   new file:   src/assets/logo.png   new file:   src/main.js   new file:   webpack.config.js

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # vue-element-tree-bug
-bug demo for element ui
+
+##
+demo for bug element-ui tree and vue-devtools
+
+steps
+
+1. install [vue-devtools 4.0.1](https://chrome.google.com/webstore/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd) from chrome webstore
+1. npm install
+1. npm run dev
+1. click `test` button in page
+
+[error](./error.png)
+
+
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <el-tree
+      :data="treeData"
+      >
+    </el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      treeData: [
+        {
+          'label': 'test',
+          'isPage': false,
+          'id': 'test'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
